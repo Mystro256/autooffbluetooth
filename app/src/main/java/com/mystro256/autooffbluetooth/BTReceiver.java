347e6537c860143ebc8098787368bd91186b9055
Mystro256 <alexjnewt@hotmail.com>
2019-02-06 13:16:33 -0500

    Debug cleanup

--- app/src/main/java/com/mystro256/autooffbluetooth/BTReceiver.java
+++ app/src/main/java/com/mystro256/autooffbluetooth/BTReceiver.java
@@ -5,7 +5,6 @@ import android.bluetooth.BluetoothProfile;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 public class BTReceiver extends BroadcastReceiver {
     @Override
