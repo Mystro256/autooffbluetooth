f9f672fce80c85021fe1194c7b87679cd1519cf2
Mystro256 <alexjnewt@hotmail.com>
2021-01-11 11:31:14 -0500

    Add check for battery optimization
    This causes the app not to work as well. Check and suggest to disable it
    via a button.
    
    As well, make use of "strings.xml" for easier future translation.

--- app/src/main/java/com/mystro256/autooffbluetooth/MainActivity.java
+++ app/src/main/java/com/mystro256/autooffbluetooth/MainActivity.java
@@ -1,13 +1,47 @@
 package com.mystro256.autooffbluetooth;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.PowerManager;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private void hideBatteryOptimization() {
+        TextView WelcomeText = findViewById(R.id.WelcomeText);
+        TextView batteryOptimizationText = findViewById(R.id.batteryOptimizationText);
+        Button batteryOptimizationButton = findViewById(R.id.batteryOptimizationButton);
+
+        WelcomeText.setVisibility(View.VISIBLE);
+        batteryOptimizationText.setVisibility(View.GONE);
+        batteryOptimizationButton.setVisibility(View.GONE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PowerManager powMan = (PowerManager) getSystemService(POWER_SERVICE);
+        String packageName = getPackageName();
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M ||
+                powMan.isIgnoringBatteryOptimizations(packageName)) {
+            hideBatteryOptimization();
+        }
+    }
+
+    public void disableBatteryOptimization(View v) {
+        Intent intent = new Intent();
+        String packageName = getPackageName();
+
+        intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+        intent.setData(Uri.parse("package:" + packageName));
+        startActivity(intent);
+        hideBatteryOptimization();
     }
 }
